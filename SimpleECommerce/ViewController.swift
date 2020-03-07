4d36ee7d1b300bc3041e714f1a6b42c8102428a1
Ahmed <aragab22@gmail.com>
2020-03-07 19:15:41 +0200

    Initial Commit

---
+++ SimpleECommerce/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleECommerce
+//
+//  Created by Ahmed Ragab on 3/7/20.
+//  Copyright © 2020 Ahmed Ragab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
